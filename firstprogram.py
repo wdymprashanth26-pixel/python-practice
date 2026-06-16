a6b222ed51b32be97a96ba9715e989ccac2f71b2
prashanth nayak <wdymprashanth26@gmail.com>
2026-06-17 03:22:13 +0530

    first commit

---
+++ firstprogram.py
@@ -0,0 +1,11 @@
+
+name = "Prashanth"
+name2 = input("Enter your name: ")
+print("Hello", name2)
+age = int(input("Enter your age: "))
+if age > 18:
+    print("Adult")
+else:
+    print("Minor") 
+    name3 = raghave
+    
