3c34e49a10bf8a2e83be2009fcc788409fe230f1
prashanth nayak <wdymprashanth26@gmail.com>
2026-06-19 20:37:51 +0530

    sum of two numbers

--- firstprogram.py
+++ firstprogram.py
@@ -1,10 +1,18 @@
+#print your details
+name = input("enter your name :")
+age = input("enter your age :")
+city = input("enter your city :")
+print("My name is", name ,"I am", age, "years old" , "i live in" , city )
+#add two numbers
+a = input("enter your first value:")
+b = input("enter your secound value:") 
+print("the sum of two numbers is :", int(a) + int(b))
+#fainding the area of rectangle
+# length = input("enter the length of rectangle:")
+width = input("enter the width of rectangle:")
+print("the area of rectangle is :", int(length) * int(width))
+#sum of two numbers 
+a = 23
+b = 27
+print(a + b)
 
-name = "Prashanth"
-name2 = input("Enter your name: ")
-print("Hello", name2)
-age = int(input("Enter your age: "))
-if age > 18:
-    print("Adult")
-else:
-    print("Minor") 
-    name3 = "Raghave"
